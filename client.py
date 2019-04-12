dff052be60583b1e725d3d24c9786c61c708bde3
thiphan <thoaptk07@gmail.com>
2019-04-12 17:07:07 -0500

    delete colors

--- client.py
+++ client.py
@@ -6,13 +6,7 @@ cipher_key = 'secret ingredient'
 
 # COLORED TEXT FUNCTIONS
 def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
-def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
-def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
-def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
-def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
 def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
-def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk))
-def prBlack(skk): print("\033[98m {}\033[00m" .format(skk))
 
 def display() :
     you="\33[33m\33[1m"+" You: "+"\33[0m"
